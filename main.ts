f08a9fcf5b438898c3871e57a45286c62b348236
Markus Karlsen <markus.karlsen@autodesk.com>
2024-07-01 15:08:23 +0200

    moving to vector representation

--- main.ts
+++ main.ts
@@ -4,21 +4,24 @@ import { range } from "taichi.js/dist/taichi";
 let main = async () => {
   await ti.init();
 
-  const n = 1000;
+  const n = 100;
+  const points = ti.Vector.field(3, ti.f32, [n, n]) as ti.Field;
   const pixels = ti.Vector.field(3, ti.f32, [n, n]) as ti.Field;
 
+  for (let i of ti.ndrange(n,n)) {
+    points.set(i,[...i,0.] );
+  }
+
   const rectangleCount = 100;
-  const rectangles = ti.field(ti.i32, [rectangleCount, 4]);
+  const rectangles = ti.Vector.field(4,ti.i32, [rectangleCount]);
 
   for (let i of range(rectangleCount)) {
     const xMin = Math.max(0, Math.random() * n - 5);
     const xMax = xMin + 5;
     const yMin = Math.max(0, Math.random() * n - 5);
     const yMax = yMin + 5;
-    rectangles.set([i, 0], xMin);
-    rectangles.set([i, 1], xMax);
-    rectangles.set([i, 2], yMin);
-    rectangles.set([i, 3], yMax);
+    const vector = [xMin, xMax, yMin, yMax];
+    rectangles.set([i], vector);
   }
 
   const analysisPointCount = 1;
@@ -31,7 +34,7 @@ let main = async () => {
     analysisPoints.set([i, 1], y);
   }
 
-  ti.addToKernelScope({ pixels, n, rectangles, rectangleCount });
+  ti.addToKernelScope({ points, pixels, n, rectangles, rectangleCount });
 
   const kernel = ti.kernel((time: number) => {
     // const goesThroughWindow = (i: number, j: number) => {
@@ -49,25 +52,25 @@ let main = async () => {
     //     return bool;
     // }
 
-    const isInside = (i: number, j: number) => {
+    const isInside = (v: ti.Vector) => {
       let bool = false;
       for (let k of ti.range(rectangleCount)) {
         if (
-          i > rectangles[(k, 0)] &&
-          i <= rectangles[(k, 1)] &&
-          j > rectangles[(k, 2)] &&
-          j <= rectangles[(k, 3)]
+          v.x > rectangles[k][0] &&
+          v.x <= rectangles[k][1] &&
+          v.y > rectangles[k][2] &&
+          v.y <= rectangles[k][3]
         ) {
           bool = true;
         }
       }
       return bool;
     };
-    for (let i of ti.ndrange(n, n)) {
-      const bool = isInside((i[0] + time * 10) % n, i[1]);
-      pixels[i][0] = bool;
-      pixels[i][1] = bool;
-      pixels[i][2] = bool;
+    for (let I of ti.ndrange(n, n)) {
+      const bool = isInside(points[I]);
+      pixels[I][0] = bool;
+      pixels[I][1] = bool;
+      pixels[I][2] = bool;
     }
   });
 
