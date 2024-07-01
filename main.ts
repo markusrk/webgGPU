9bb9aa97de25d1bc921f317c42035605fd370de7
Markus Karlsen <markus.karlsen@autodesk.com>
2024-07-01 13:50:11 +0200

    random updates

--- main.ts
+++ main.ts
@@ -4,14 +4,16 @@ import { range } from "taichi.js/dist/taichi";
 let main = async () => {
   await ti.init();
 
-  const n = 2000;
+  const n = 100;
   const pixels = ti.Vector.field(3, ti.f32, [n, n]) as ti.Field;
-  const rectangles = ti.field(ti.i32, [n, 4]);
+  
+  const rectangleCount = 10000
+  const rectangles = ti.field(ti.i32, [rectangleCount, 4]);
 
-  for (let i of range(n)) {
-    const xMin = Math.random() * n + 5;
+  for (let i of range(rectangleCount)) {
+    const xMin = Math.max(0,Math.random() * n - 5);
     const xMax = xMin + 5;
-    const yMin = Math.random() * n + 5;
+    const yMin = Math.max(0,Math.random() * n - 5)
     const yMax = yMin + 5;
     rectangles.set([i, 0], xMin);
     rectangles.set([i, 1], xMax);
@@ -20,28 +22,28 @@ let main = async () => {
   }
   console.log(rectangles);
 
-  ti.addToKernelScope({ pixels, n, rectangles });
+  ti.addToKernelScope({ pixels, n, rectangles, rectangleCount });
 
   const kernel = ti.kernel((time: number) => {
     const isInside = (i: number,j: number) => {
       let bool = false;
-      for (let k of ti.range(n)) {
+      for (let k of ti.range(rectangleCount)) {
         if (i > rectangles[k,0] && i <= rectangles[k,1] && j >rectangles[k,2] && j <= rectangles[k,3]) {
           bool = true;
         }
       }
       return bool;
     };
-    for (let i of ti.ndrange(n, n)) {
-      let color = 1 + ti.sin((i[0] + i[1] + time) / 10);
-      pixels[i][0] = isInside(i[0],i[1]);
-      pixels[i][1] = isInside(i[0],i[1]);
-      pixels[i][2] = isInside(i[0],i[1]);
+    for (let i of ti.ndrange(n,n)) {
+        const bool = isInside((i[0]+time*10)%n,i[1]);
+      pixels[i][0] = bool;
+      pixels[i][1] = bool;
+      pixels[i][2] = bool;
     }
   });
 
   const htmlCanvas = document.getElementById("result_canvas")! as ti.Canvas;
-  htmlCanvas.width = 2 * n;
+  htmlCanvas.width =  n;
   htmlCanvas.height = n;
   let canvas = new ti.Canvas(htmlCanvas);
 
