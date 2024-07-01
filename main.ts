9704167c6435d8ac73bc8343b80e7ba0b84b75ad
Markus Karlsen <markus.karlsen@autodesk.com>
2024-07-01 12:53:19 +0200

    adding for-loop function to set color

--- main.ts
+++ main.ts
@@ -1,48 +1,57 @@
-import * as ti from "taichi.js"
+import * as ti from "taichi.js";
+import { range } from "taichi.js/dist/taichi";
 
 let main = async () => {
-    await ti.init();
+  await ti.init();
 
-    let n = 200;
-    let pixels = ti.Vector.field(3, ti.f32, [n, n]) as ti.Field;
-    
-    
-    
-    ti.addToKernelScope({ pixels, n });
-    
-    const kernel = ti.kernel((time: number) => {  
-        const isInside = (i: number, j: number) => {
-            let bool = true
-            if (i < 0 || i >= n || j < 0 || j >= n/2) {
-                bool = false;
-            }
-            return bool
-        }
+  const n = 2000;
+  const pixels = ti.Vector.field(3, ti.f32, [n, n]) as ti.Field;
+  const rectangles = ti.field(ti.i32, [n, 4]);
 
-        for (let i of ti.ndrange(n, n)) {
-            let color = (1+ti.sin((i[0]+i[1]+time)/10));
-            pixels[i][0] = isInside(i[0], i[1]);
-            pixels[i][1] = (1+ti.cos((i[0]+i[1]+time)/10));
-            pixels[i][2] = (1+ti.cos((i[0]-i[1]+time)/10));
+  for (let i of range(n)) {
+    const xMin = Math.random() * n + 5;
+    const xMax = xMin + 5;
+    const yMin = Math.random() * n + 5;
+    const yMax = yMin + 5;
+    rectangles.set([i, 0], xMin);
+    rectangles.set([i, 1], xMax);
+    rectangles.set([i, 2], yMin);
+    rectangles.set([i, 3], yMax);
+  }
+  console.log(rectangles);
+
+  ti.addToKernelScope({ pixels, n, rectangles });
+
+  const kernel = ti.kernel((time: number) => {
+    const isInside = (i: number,j: number) => {
+      let bool = false;
+      for (let k of ti.range(n)) {
+        if (i > rectangles[k,0] && i <= rectangles[k,1] && j >rectangles[k,2] && j <= rectangles[k,3]) {
+          bool = true;
         }
       }
-    )
-
-
-
-
-    const htmlCanvas = document.getElementById('result_canvas')! as ti.Canvas
-    htmlCanvas.width = 2 * n;
-    htmlCanvas.height = n;
-    let canvas = new ti.Canvas(htmlCanvas);
-
-    let i = 0;
-    async function frame() {
-        i = i + 1;
-        kernel(i);
-        canvas.setImage(pixels);
-        requestAnimationFrame(frame);
+      return bool;
+    };
+    for (let i of ti.ndrange(n, n)) {
+      let color = 1 + ti.sin((i[0] + i[1] + time) / 10);
+      pixels[i][0] = isInside(i[0],i[1]);
+      pixels[i][1] = isInside(i[0],i[1]);
+      pixels[i][2] = isInside(i[0],i[1]);
     }
+  });
+
+  const htmlCanvas = document.getElementById("result_canvas")! as ti.Canvas;
+  htmlCanvas.width = 2 * n;
+  htmlCanvas.height = n;
+  let canvas = new ti.Canvas(htmlCanvas);
+
+  let i = 0;
+  async function frame() {
+    i = i + 1;
+    kernel(i);
+    canvas.setImage(pixels);
     requestAnimationFrame(frame);
+  }
+  requestAnimationFrame(frame);
 };
 main();
