a9857d55ce602ca9412c6f5266a5edf917ea2a50
Markus Karlsen <markus.karlsen@autodesk.com>
2024-07-02 14:03:12 +0200

    typscript fix

--- geometryTools.ts
+++ geometryTools.ts
@@ -1,4 +1,6 @@
+import * as ti from "taichi.js";
 
-export const isPointWithinRectangle = (point: [number,number], polygon: [number,number][]): boolean => {
+export const isPointWithinRectangle = (point: ti.Vector, polygon: ti.Vector[]): boolean => {
     return point.x >= polygon[0].x && point.x <= polygon[2].x && point.y >= polygon[0].y && point.y <= polygon[2].y;
 }
+
