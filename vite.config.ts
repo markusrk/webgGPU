857643505165561e695c49b1e477297237bfc8e9
Markus Karlsen <markus.karlsen@autodesk.com>
2024-07-08 12:39:56 +0200

    build library

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,11 @@ import taichi from "rollup-plugin-taichi";
 export default defineConfig({
   base: "/webgGPU/dist/",
   build: {
+    lib: {
+      entry: "src.rayTracer.ts",
+      name: "daylightTracer",
+      fileName: "daylightTracer",
+    },
     minify: false,
     watch: {},
     commonjsOptions: { include: [] },
