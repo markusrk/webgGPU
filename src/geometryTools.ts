75163210920a9cb61c4e47393db4029f561daa36
Markus Karlsen <markus.karlsen@autodesk.com>
2024-07-10 17:42:29 +0200

    exporting geometry tools

--- src/geometryTools.ts
+++ src/geometryTools.ts
@@ -22,7 +22,7 @@ export const generateWindowsAlongWall = (
   for (let i = 0; i < polygon.length - 1; i++) {
     const startPosition = polygon[i];
     const endPosition = polygon[i + 1];
-    const relDir = minus(endPosition, startPosition);
+    const relDir = sub(endPosition, startPosition);
     const dir = norm(relDir);
     const wallLength = Math.sqrt(
       Math.pow(endPosition[0] - startPosition[0], 2) + Math.pow(endPosition[1] - startPosition[1], 2)
@@ -43,23 +43,26 @@ export const generateWindowsAlongWall = (
   return windows;
 };
 
-const minus = (a: number[], b: number[]): number[] => {
-  return a.map((_, i) => a[i] - b[i]);
+export type Point = [number, number, number];
+
+
+export const sub = (a: Point, b: Point): Point => {
+  return a.map((_, i) => a[i] - b[i]) as Point;
 };
 
-const norm = (a: number[]): number[] => {
+const norm = (a: Point): Point => {
   const mag = Math.sqrt(a.reduce((acc, cur) => acc + cur * cur, 0));
-  return a.map((x) => x / mag);
+  return a.map((x) => x / mag) as Point;
 };
 
-const add = (a: number[], b: number[]): number[] => {
-  return a.map((_, i) => a[i] + b[i]);
+export const add = (a: Point, b: Point): Point => {
+  return a.map((_, i) => a[i] + b[i]) as Point;
 };
 
-const mul = (a: number[], b: number[]): number[] => {
-  return a.map((_, i) => a[i] * b[i]);
+const mul = (a: Point, b: Point): Point => {
+  return a.map((_, i) => a[i] * b[i]) as Point;
 };
 
-const scalarMul = (a: number[], b: number): number[] => {
-  return a.map((x) => x * b);
+const scalarMul = (a: Point, b: number): Point => {
+  return a.map((x) => x * b) as Point;
 };
