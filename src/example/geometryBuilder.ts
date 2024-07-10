b35edf97116ad6c3ec9e7ff0a49c4baa1e9b6cdd
Markus Karlsen <markus.karlsen@autodesk.com>
2024-07-10 17:48:21 +0200

    WIP on a box builder

---
+++ src/example/geometryBuilder.ts
@@ -0,0 +1,22 @@
+import { add, Point, sub } from "../geometryTools";
+
+type Triangle = [Point, Point, Point];
+
+const wallTrianglesFromPoints = (p1: Point, p2: Point): [Triangle,Triangle] => {
+    const c1 = [p1[0], p1[1], p2[2]] as Point;
+    const c2 = [p2[0], p2[1], p1[2]] as Point;
+    return [[p1,c2,p2],[p1,p2,c1]];
+};
+
+export const boxFromAABB = (p1: Point, p2: Point): Triangle[] => {
+    const x = [p2[0]-p1[0],0,0] as Point;
+    const y = [0,p2[1]-p1[1],0] as Point;
+    const z = [0,0,p2[2]-p1[2]] as Point;
+
+    const front = wallTrianglesFromPoints(p1, add(add(p1,x),z));
+    const right = wallTrianglesFromPoints(add(p1,x), p2);
+    const back = wallTrianglesFromPoints(sub(p2,z), add(p2,x));
+    const left = wallTrianglesFromPoints(add(p1,z), add(p1,y));
+
+    return [...front, ...right, ...back, ...left];
+}
