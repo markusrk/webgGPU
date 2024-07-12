d141e55acd78de5706614ac6361b11905be47d9c
Markus Karlsen <markus.karlsen@autodesk.com>
2024-07-12 13:28:02 +0200

    fixes in geometry builder

--- src/example/geometryBuilder.ts
+++ src/example/geometryBuilder.ts
@@ -11,11 +11,10 @@ const wallTrianglesFromPoints = (p1: Point, p2: Point): [Triangle, Triangle] => 
   ];
 };
 
-const roofTrianglesFromPoints = (p1: Point, p2: Point): Triangle[] => {
+const downwardsFacingTrianglesFromPoints = (p1: Point, p2: Point): Triangle[] => {
     const c1 = [p2[0], p1[1], p2[2]] as Point;
     const c2 = [p1[0], p2[1], p2[2]] as Point;
     return [[p1, c2,p2], [p1, p2,c1]];
-    
 }
 
 export const inwardsBoxFromAABB = (p1: Point, p2: Point): Triangle[] => {
@@ -27,7 +26,7 @@ export const inwardsBoxFromAABB = (p1: Point, p2: Point): Triangle[] => {
   const right = wallTrianglesFromPoints(sub(p2,z), sub(p2, y));
   const back = wallTrianglesFromPoints(add(p1, y), p2);
   const left = wallTrianglesFromPoints(p1, add(add(p1, z),y))
-  const top = roofTrianglesFromPoints( add(p1, z),p2);
+  const top = downwardsFacingTrianglesFromPoints( add(p1, z),p2);
 
   return [...front, ...right, ...back, ...left, ...top];
 };
@@ -42,6 +41,8 @@ export const boxFromAABB = (p1: Point, p2: Point): Triangle[] => {
   const right = wallTrianglesFromPoints(add(p1, x), p2);
   const back = wallTrianglesFromPoints(sub(p2, z), sub(p2, x));
   const left = wallTrianglesFromPoints(add(p1, y), add(p1, z));
+  const bottom = downwardsFacingTrianglesFromPoints( p1,sub(p2,z));
 
-  return [...front, ...right, ...back, ...left];
+
+  return [...front, ...right, ...back, ...left, ...bottom];
 };
