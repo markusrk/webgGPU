9a1daba9c29ab941e41588aa66a128dc4e3c8c13
Markus Karlsen <markus.karlsen@autodesk.com>
2024-07-21 18:19:40 +0200

    lock sky value at zero for downwards-facing rays

--- src/sky.ts
+++ src/sky.ts
@@ -1,5 +1,10 @@
 import * as ti from "taichi.js";
 
+// todo: Currently, this function forces the score to zero for downward rays. It is not given that this is the correct behavior.
 export const getSpecificVCSScoreAtRay = ti.func((ray: ti.Vector) => {
-  return 1.0 + 2.0 * ray[2];
+  let res = 1.0 + 2.0 * ray[2];
+  if (ray[2] <= 0) {
+    res = 0;
+  }
+  return res
 });
