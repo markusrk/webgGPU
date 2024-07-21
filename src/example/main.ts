e8948c53d6ef3b0f55a25302f9589df31cde405a
Markus Karlsen <markus.karlsen@autodesk.com>
2024-07-21 18:28:42 +0200

    fix interaction update errors

--- src/example/main.ts
+++ src/example/main.ts
@@ -5,9 +5,9 @@ import { boxFromAABBWithHoleInTheTop, inwardsBoxFromAABBWithwindow } from "./geo
 
 const resolution = 1000;
 let defaultWindowOptions = { windowSize: 50, windowSpacing: 200, windowHeight: 100 };
-let bounceOptions = {materialReflectivity: 0.2, maxBounces: 6}
-let windowWidth = 0.2;
-let windowHeight = 0.2;
+let bounceOptions = { materialReflectivity: 0.1, maxBounces: 6 };
+let windowWidth = 0.1;
+let windowHeight = 0.1;
 
 let polygonInJS = [
   [resolution * 0.1, resolution * 0.1],
@@ -18,22 +18,25 @@ let polygonInJS = [
 ] as [number, number][];
 
 let wallsInJs = [
-  ...boxFromAABBWithHoleInTheTop([resolution * 0.1, resolution * 0.1, 0], [resolution * 0.9, resolution * 0.9, 400]),
+  // ...boxFromAABBWithHoleInTheTop([resolution * 0.1, resolution * 0.1, 0], [resolution * 0.9, resolution * 0.9, 400]),
   ...inwardsBoxFromAABBWithwindow(
     [resolution * 0.1, resolution * 0.1, 0],
-    [resolution * 0.9, resolution * 0.9, 400],windowWidth,windowHeight
+    [resolution * 0.9, resolution * 0.9, 1000],
+    windowWidth,
+    windowHeight
   ),
 ];
-
 
 document.getElementById("windowSize")!.addEventListener("input", (e) => {
   const v = (e.target as HTMLInputElement).value;
   windowWidth = parseFloat(v);
   wallsInJs = [
-    ...boxFromAABBWithHoleInTheTop([resolution * 0.1, resolution * 0.1, 0], [resolution * 0.9, resolution * 0.9, 400]),
+    // ...boxFromAABBWithHoleInTheTop([resolution * 0.1, resolution * 0.1, 0], [resolution * 0.9, resolution * 0.9, 400]),
     ...inwardsBoxFromAABBWithwindow(
       [resolution * 0.1, resolution * 0.1, 0],
-      [resolution * 0.9, resolution * 0.9, 400],windowWidth,windowHeight
+      [resolution * 0.9, resolution * 0.9, 400],
+      windowWidth,
+      windowHeight
     ),
   ];
   updateImage(polygonInJS, wallsInJs, bounceOptions);
@@ -42,10 +45,12 @@ document.getElementById("windowHeight")!.addEventListener("input", (e) => {
   const v = (e.target as HTMLInputElement).value;
   windowHeight = parseFloat(v);
   wallsInJs = [
-    ...boxFromAABBWithHoleInTheTop([resolution * 0.1, resolution * 0.1, 0], [resolution * 0.9, resolution * 0.9, 400]),
+    // ...boxFromAABBWithHoleInTheTop([resolution * 0.1, resolution * 0.1, 0], [resolution * 0.9, resolution * 0.9, 400]),
     ...inwardsBoxFromAABBWithwindow(
       [resolution * 0.1, resolution * 0.1, 0],
-      [resolution * 0.9, resolution * 0.9, 400],windowWidth,windowHeight
+      [resolution * 0.9, resolution * 0.9, 400],
+      windowWidth,
+      windowHeight
     ),
   ];
   updateImage(polygonInJS, wallsInJs, bounceOptions);
@@ -53,14 +58,14 @@ document.getElementById("windowHeight")!.addEventListener("input", (e) => {
 document.getElementById("reflectivityInput")!.addEventListener("input", (e) => {
   const v = (e.target as HTMLInputElement).value;
   bounceOptions = { ...bounceOptions, materialReflectivity: parseFloat(v) };
-  updateImage(polygonInJS, defaultWindowOptions, bounceOptions);
+  updateImage(polygonInJS, wallsInJs, bounceOptions);
 });
 document.getElementById("maxBouncesInput")!.addEventListener("input", (e) => {
   const v = (e.target as HTMLInputElement).value;
   bounceOptions = { ...bounceOptions, maxBounces: parseInt(v) };
 
-  updateImage(polygonInJS, defaultWindowOptions, bounceOptions);
- });
+  updateImage(polygonInJS, wallsInJs, bounceOptions);
+});
 
 const updateCoordinate = (x, y) => {
   polygonInJS = [
@@ -73,9 +78,8 @@ const updateCoordinate = (x, y) => {
   updateImage(polygonInJS, defaultWindowOptions);
 };
 
-const updateImage = (polygonInJS, windowOptions, bounceOptions) => {
-  const windowsInJS = generateWindowsAlongWall(polygonInJS, windowOptions, );
-  rayTrace(polygonInJS, wallsInJs, bounceOptions);
+const updateImage = (polygonInJS, wallsInJS, bounceOptions) => {
+  rayTrace(polygonInJS, wallsInJS, bounceOptions);
   // worker.postMessage({type: "update", polygon: polygonInJS});
 };
 
@@ -87,10 +91,8 @@ const htmlCanvas = document.getElementById("result_canvas")! as ti.Canvas;
 htmlCanvas.width = resolution;
 htmlCanvas.height = resolution;
 
-
 const main = async () => {
   await init(htmlCanvas, resolution);
-  const windowsInJS = generateWindowsAlongWall(polygonInJS, defaultWindowOptions);
   rayTrace(polygonInJS, wallsInJs, bounceOptions);
 };
 main();
