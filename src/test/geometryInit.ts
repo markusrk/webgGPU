ab5b44d05733bd1c76fd259a412cf2bc8d2bb488
Markus Karlsen <markus.karlsen@autodesk.com>
2024-07-23 14:21:57 +0200

    use local constant in geometryInit

--- src/test/geometryInit.ts
+++ src/test/geometryInit.ts
@@ -4,12 +4,12 @@ export const initRandomVertices = async (triangleCount: number) => {
   const vertices = ti.Vector.field(3, ti.f32, [triangleCount * 3]) as ti.field;
   const indices = ti.Vector.field(3, ti.i32, [triangleCount]) as ti.field;
 
-  ti.addToKernelScope({ vertices, indices });
+  ti.addToKernelScope({ vertices, indices, triangleCount });
 
   const kernel = ti.kernel(() => {
     const scale = 1000;
     const smallScale = 10;
-    for (let i of ti.range(M)) {
+    for (let i of ti.range(triangleCount)) {
       const step = i * 3;
       vertices[step] = [ti.random() * scale, ti.random() * scale, ti.random() * scale];
       vertices[step + 1] = vertices[step] + [ti.max(ti.random(), 0.1) * smallScale, 0, 0];
