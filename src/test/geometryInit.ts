b52fa5c158a041aa916ba3c0e8457552bcb82fd5
Markus Karlsen <markus.karlsen@autodesk.com>
2024-07-29 18:33:45 +0200

    bugfix missing commit on a few lines

--- src/test/geometryInit.ts
+++ src/test/geometryInit.ts
@@ -1,25 +1,24 @@
 import * as ti from "taichi.js";
 
-export const initRandomVertices = async (triangleCount: number) => {
+export const initRandomVertices = async (triangleCount: number, scale: number) => {
   const vertices = ti.Vector.field(3, ti.f32, [triangleCount * 3]) as ti.field;
   const indices = ti.Vector.field(3, ti.i32, [triangleCount]) as ti.field;
 
   ti.addToKernelScope({ vertices, indices, triangleCount });
 
-  const kernel = ti.kernel(() => {
-    const scale = 1000;
-    const scaleZ = 50
+  const kernel = ti.kernel((scale) => {
+    const scaleZ = 50;
     const smallScale = 10;
     for (let i of ti.range(triangleCount)) {
       const step = i * 3;
       vertices[step] = [ti.random() * scale, ti.random() * scale, ti.random() * scaleZ];
-      vertices[step + 2] = vertices[step] + [(ti.random()-0.5) * smallScale, 0, (ti.random()-0.5) * smallScale];
-      vertices[step + 1] = vertices[step] + [0, (ti.random()-0.5) * smallScale, (ti.random()-0.5) * smallScale];
+      vertices[step + 2] = vertices[step] + [(ti.random() - 0.5) * smallScale, 0, (ti.random() - 0.5) * smallScale];
+      vertices[step + 1] = vertices[step] + [0, (ti.random() - 0.5) * smallScale, (ti.random() - 0.5) * smallScale];
       indices[i] = [step, step + 1, step + 2];
     }
   });
 
-  kernel();
+  kernel(scale);
 
   return { vertices, indices };
 };
